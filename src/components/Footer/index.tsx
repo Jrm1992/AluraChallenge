5b84ae95a0070a9564026abe3ae7bcf6d69a5b8d
Jose <jose_roberto_6@msn.com>
2022-09-28 19:19:39 -0300

    add Footer Component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Button from '../Button'
+import Logo from '../Logo/logo'
+
+export default function Footer() {
+  return (
+    <div className='w-ful h-[470px] flex flex-col'>
+      <div className='w-full h-[368px] bg-[#EAF2FD] flex flex-row justify-between py-16 px-40 '>
+        <Logo />
+        <div className='w-48 h-full justify-between flex flex-col'>
+          <a>Quem somos nós</a>
+          <a>Política de privacidade</a>
+          <a>Programa fidelidade</a>
+          <a>Nossas lojas</a>
+          <a>Quero ser franqueado</a>
+          <a>Anuncie aqui</a>
+        </div>
+        <form className='w-[560px] h-full flex flex-col gap-2 '>
+          <span className='font-bold text-[#464646]'>Fale Conosco</span>
+          <input className='h-14 rounded shadow outline-none' />
+          <input className='h-20 rounded shadow outline-none' />
+          <div>
+            <Button type='submit' Text='Enviar mensagem' />
+          </div>
+        </form>
+      </div>
+      <div className='w-full flex flex-col justify-center items-center p-8'> 
+        <span>Desenvolvido por Jose Roberto Marques</span>
+        <span>2022</span>
+      </div>
+    </div>
+  )
+}
